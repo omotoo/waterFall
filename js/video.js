2e5f5ed240fcdb4fa736868a73128fb9a10cf187
wax0329 <2734954695@qq.com>
2016-12-22 11:07:23 +0800

    1

--- js/video.js
+++ js/video.js
@@ -25,26 +25,89 @@ var browser = {
 	language: (navigator.browserLanguage || navigator.language).toLowerCase()
 }
 $(function(){
-	getVideo();
+	getVideo({
+		id:'.video_content',
+		firstLoadRows:3
+	});
 });
-function findParent(el){
-	return $(el).parent('.video');
+function getLastOneTop(lastone){
+	var top=lastone.offsetTop;
+	while(lastone.offsetParent!=null){
+		lastone=lastone.offsetParent;
+		top+=lastone.offsetTop;
+	}
+	return top;
+}
+function getLastOneBottom(parent){
+	var lastone=getLastOne(parent);
+	var top=getLastOneTop(lastone)+lastone.offsetHeight;
+	var clientHeight=document.documentElement.clientHeight;
+	if(clientHeight+window.scrollY>=top){
+		return true;
+	}else{
+		return false;
+	}
 }
 
-// 获取视频瀑布流，开始加载一屏约5行
-function getVideo(){
+function getLastOne(parent){
+	var childrenArr=parent.children();
+	var lastOneIndex=childrenArr.length-1;
+	return childrenArr[lastOneIndex];
+}
+
+function addElement(alreadyLoad,rows,data,parent){
+	var arr=[];
+	for (var i = alreadyLoad; i < alreadyLoad+rows; i++) {
+		var template = "<div class='video'><div class='pic'><img src=" + data[i].videoPicture + " /></div><p class='video_t'>" + data[i].videoName + "</p></div>";
+		arr.push(template);
+	}
+	parent.html(parent.html()+arr.join(''));
+}
+
+function getVideo(obj){
+	if(obj){
+		var id=obj.id;
+		var alreadyLoad=0;
+		var rows=obj.firstLoadRows;
+	}
+	var videoContent=$(id);
+	var flag=true;
 	$.ajax({
 		url: 'video.json',
 		type: 'GET',
 		success:function(data){
-			//var data=$.parseJSON(data);
-			var videoContent=$('.video_content');
-			var arr=[];
-			for(var i=0;i<data.length;i++){
-				var template="<div class='video'><div class='pic'><img src="+data[i].videoPicture+" /></div><p class='video_t'>"+data[i].videoName+"</p></div>";
-				arr.push(template);
+			//var data=$.parseJSON(data);			
+			// 首次加载列数根据页面高度决定
+			while(flag){				
+				if(data.length-alreadyLoad<=rows){
+					rows=data.length-alreadyLoad;
+					flag=false;
+				}				
+				addElement(alreadyLoad,rows,data,videoContent);				
+				alreadyLoad+=rows;
+				if(flag){					
+					flag=getLastOneBottom(videoContent);
+					// console.log(flag);
+				}					
+			};	
+			// 瀑布流			
+			window.onscroll=function(){
+				if(getLastOneBottom(videoContent)){
+					// console.log('到底');
+					var add=0;						
+					while(rows){
+						if(data.length-alreadyLoad>=rows){
+							add=rows;
+							break;							
+						}			
+						rows--;			
+					}
+					addElement(alreadyLoad,add,data,videoContent);	
+					alreadyLoad+=add;
+					// console.log(alreadyLoad);
+				}
 			}
-			videoContent.html(arr.join(''));
 		}
 	});	
+
 }
